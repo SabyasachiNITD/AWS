f59aa310dc606bab05ddd357d65e357853628571
SabyasachiNITD <sabyasachi9733@gmail.com>
2024-02-07 13:50:45 +0530

    DynamoDB Lambda created

--- DynamoDBLambdaIntegration/myLambda/app.py
+++ DynamoDBLambdaIntegration/myLambda/app.py
@@ -19,22 +19,89 @@ def lambda_handler(event,context):
     httpMethod = event['httpMethod']
     requestBody = json.loads(event['body'])
     if httpMethod == get:
-        response = getUserId(event['queryStringParameters']['userId'])
+        response = getUser(event['queryStringParameters']['userId'])
     elif httpMethod == post:
-        response = saveUserId(json.loads(event['body']))
+        response = saveUser(json.loads(event['body']))
     elif httpMethod == patch:
-        response = updateUserId(requestBody['userId'],requestBody['updateKey'],requestBody['updateValue'])
+        response = updateUser(requestBody['userId'],requestBody['updateKey'],requestBody['updateValue'])
     elif httpMethod == delete:
-        response = deleteUserId(requestBody['userId'])
+        response = deleteUser(requestBody['userId'])
     else:
         response = buildResponse(404,'Not Found')
 
     return response
 
-def getUserId(userId):
+def getUser(userId):
     try:
         response = table.get_item(Key= {'userId':userId})
-    if 'Item' in response:
-        return bu
+        if 'Item' in response:
+            return buildResponse(200,response['Item'])
+        else:
+            return buildResponse(404,{'message' : 'userId %s not found'} %userId)
+    except:
+        logger.exception("Unknown error while fetching")
+
+def saveUser(requestBody):
+    try:
+        table.put_item(Item=requestBody)
+        body = {
+            'Operation' : 'CREATE',
+            'Message' : 'SUCCESS',
+            'Item' : requestBody
+        }
+        return buildResponse(200,body)
+    except:
+        logger.exception("Unknown error while creating item")
+
+def updateUser(userId,updateKey,updateValue):
+    try:
+        response = table.update_item(
+            Key = {
+                'userId' : userId
+            },
+            UpdateExpression = 'SET %s = :val1' %updateKey,
+            ExpressionAttributeValues = {
+                ':val1' : updateValue
+            },
+            ReturnValues = 'UPDATED_NEW'
+        )
+        body = {
+            'Operation' : 'UPDATE',
+            'Message' : 'SUCCESS',
+            'UpdatedAttributes' : response
+        }
+        return buildResponse(200,body)
+    except:
+        logger.exception('Unknown Error while Updating')
+
+def deleteUser(userId):
+    try:
+        response = table.delete_item(
+            Key = {
+                'userId' : userId
+            },
+            ReturnValues = 'ALL_OLD'
+        )
+        body = {
+            'Operation': 'UPDATE',
+            'Message': 'SUCCESS',
+            'UpdatedAttributes': response
+        }
+
+        return buildResponse(200,body)
+    except:
+        logger.exception("Unknown error while deleting")
+def buildResponse(statusCode,body=None):
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body)
+    return response
+
 
 
